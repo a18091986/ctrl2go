6c713a46f343fff014add3fdf317d24953e574eb
behappyman <a18091986>
2023-12-23 18:13:21 +0300

    hist

--- weather_hist/main.py
+++ weather_hist/main.py
@@ -1,52 +1,86 @@
+import pandas as pd
+import dotenv
 import requests
 import json
+from typing import Tuple
 
-#  Получение данных о погоде
-meteo_api_url = "http://192.168.0.10:8084/meteo/api/v1/webportal/interpolPointParamsMap?endDate=03-01-2019&startDate=01-01-2019"
-response = requests.get(meteo_api_url)
+env = dotenv.dotenv_values('../.env')
+auth_key = env.get("AUTH_TOKEN")
 
-if response.status_code != 200:
-    print(f"Error: Unable to fetch meteo data. {response.content}")
-    exit()
 
-meteo_data = response.json()
+def get_history_weather(datamode: str = 'bilinear', src_id: int = 2,
+                        geo_points: Tuple[Tuple[float | int, float | int], ...] = ((28.632854, 77.219721),),
+                        start_date: str = "1990-01-01", end_date: str = "1990-01-03",
+                        out_format: str = 'csv') -> pd.DataFrame | None:
+    """
 
-#  Обработка данных
-datahub_data = {
-    "data": []
-}
-for item in meteo_data.get("data", []):
-    transformed_item = {
-        "project": item.get("project", 1),
-        "subproject": item.get("subproject", 2),
-        "latitude": item.get("latitude"),
-        "longitude": item.get("longitude"),
-        "id_state": item.get("id_state"),
-        "id_district": item.get("id_district"),
-        "id_block": item.get("id_block"),
-        "id_gp": item.get("id_gp"),
-        "division_level": item.get("division_level"),
-        "date": item.get("date"),
-        "mean_temp": item.get("mean_temp"),
-        "max_temp": item.get("max_temp"),
-        "min_temp": item.get("min_temp"),
-        "prec": item.get("prec"),
-        "mean_rh": item.get("mean_rh"),
-        "max_rh": item.get("max_rh"),
-        "min_rh": item.get("min_rh"),
-        "rad": item.get("rad")
+    Parameters
+    ----------
+    datamode
+    src_id
+    geo_points
+    start_date
+    end_date
+    out_format
+
+    Returns
+    -------
+
+    """
+    weather_hist = pd.DataFrame()
+    base_url = "http://212.41.22.36:8084/meteo/api/v1/webportal/ptDailyMeteoData"
+    parameters = f"srcId={src_id}&dataMode={datamode}&startDate={start_date}&endDate={end_date}"
+    url = '?'.join([base_url, parameters])
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {auth_key}'
     }
-    datahub_data["data"].append(transformed_item)
+
+    # формируем тело запроса из набора координат
+    geo_points_dict = list(map(lambda x: dict(latitude=x[0], longitude=x[1]), geo_points))
+    geo_points_json = json.dumps(geo_points_dict)
+
+    response = requests.request("POST", url, headers=headers, data=geo_points_json)
+
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch meteo data. {response.content}")
+        return weather_hist
+
+    weather_hist = pd.DataFrame.from_records(response.json())
+    weather_hist = weather_hist.loc[:, ~weather_hist.columns.isin(['sourceId', 'isInterpolated'])]
+
+    if out_format == 'xlsx':
+        weather_hist.to_excel('test.xlsx')
+    elif out_format == 'sql':
+        pass
+
+    return weather_hist
+
+
+if __name__ == '__main__':
+    geo_points = (
+        (21.883583, 77.205752),
+        (23.883583, 80.205752),
+        (17.883583, 74.205752)
+    )
+
+    weather = get_history_weather(geo_points=geo_points, start_date='1990-01-01', end_date='1990-02-01',
+                                  out_format='xlsx')
+    weather = weather.set_index(['latitude', 'longitude'])
+    print(weather.head(10).to_string())
+
+
 
 # . Отправка данных в datahub
-datahub_api_url = "http://212.41.22.38:8094/api/v1/weather-data/daily"
-headers = {
-    "Content-Type": "application/json"
-}
-
-response = requests.post(datahub_api_url, data=json.dumps(datahub_data), headers=headers)
-
-if response.status_code == 200:
-    print("Success: Records created in datahub")
-else:
-    print(f"Error: Unable to create records in datahub. {response.content}")
+# datahub_api_url = "http://212.41.22.38:8094/api/v1/weather-data/daily"
+# headers = {
+#     "Content-Type": "application/json"
+# }
+#
+# response = requests.post(datahub_api_url, data=json.dumps(datahub_data), headers=headers)
+#
+# if response.status_code == 200:
+#     print("Success: Records created in datahub")
+# else:
+#     print(f"Error: Unable to create records in datahub. {response.content}")
