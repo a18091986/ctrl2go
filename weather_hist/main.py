e383168bcce1d8192cb54ad5a34d6fc8488e6027
behappyman <a18091986>
2023-12-24 08:59:24 +0300

    weather_hist

--- weather_hist/main.py
+++ weather_hist/main.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+import numpy as np
 import pandas as pd
+from pathlib import Path
 import dotenv
 import requests
 import json
@@ -13,21 +17,23 @@ def get_history_weather(datamode: str = 'bilinear', src_id: int = 2,
                         start_date: str = "1990-01-01", end_date: str = "1990-01-03",
                         out_format: str = 'csv') -> pd.DataFrame | None:
     """
-
     Parameters
     ----------
-    datamode
-    src_id
-    geo_points
-    start_date
-    end_date
-    out_format
+    datamode - интерполяция
+    src_id - источник данных
+    geo_points - набор кортежей широта-долгота географических точек
+    start_date - начальная дата
+    end_date - конечная дата
+    out_format - выходной формат данных: csv, xlsx или загрузка в БД
 
     Returns
     -------
 
     """
     weather_hist = pd.DataFrame()
+    dir_to_save = Path("out_data")
+    dir_to_save.mkdir(exist_ok=True)
+
     base_url = "http://212.41.22.36:8084/meteo/api/v1/webportal/ptDailyMeteoData"
     parameters = f"srcId={src_id}&dataMode={datamode}&startDate={start_date}&endDate={end_date}"
     url = '?'.join([base_url, parameters])
@@ -51,36 +57,31 @@ def get_history_weather(datamode: str = 'bilinear', src_id: int = 2,
     weather_hist = weather_hist.loc[:, ~weather_hist.columns.isin(['sourceId', 'isInterpolated'])]
 
     if out_format == 'xlsx':
-        weather_hist.to_excel('test.xlsx')
+        weather_hist.to_excel(Path(dir_to_save, f"weather_{datetime.now().strftime('%Y%m%d__%H%M%S')}.xlsx"))
+        exit()
+
     elif out_format == 'sql':
         pass
+        exit()
 
+    weather_hist.to_csv(Path(dir_to_save, f"weather_{datetime.now().strftime('%Y%m%d__%H%M%S')}.csv"))
     return weather_hist
 
 
 if __name__ == '__main__':
-    geo_points = (
-        (21.883583, 77.205752),
-        (23.883583, 80.205752),
-        (17.883583, 74.205752)
-    )
 
-    weather = get_history_weather(geo_points=geo_points, start_date='1990-01-01', end_date='1990-02-01',
-                                  out_format='xlsx')
-    weather = weather.set_index(['latitude', 'longitude'])
-    print(weather.head(10).to_string())
+    # geo_points_test = (
+    #     (21.883583, 77.205752),
+    #     (23.883583, 80.205752),
+    #     (17.883583, 74.205752)
+    # )
 
+    geo_points_test = tuple((x, y) for x in np.arange(20, 21, 0.25) for y in np.arange(77, 78, 0.25))
+    start_date = '1990-01-01'
+    end_date = '1990-01-03'
 
+    weather = get_history_weather(geo_points=geo_points_test, start_date=start_date, end_date=end_date,
+                                  out_format='csv')
 
-# . Отправка данных в datahub
-# datahub_api_url = "http://212.41.22.38:8094/api/v1/weather-data/daily"
-# headers = {
-#     "Content-Type": "application/json"
-# }
-#
-# response = requests.post(datahub_api_url, data=json.dumps(datahub_data), headers=headers)
-#
-# if response.status_code == 200:
-#     print("Success: Records created in datahub")
-# else:
-#     print(f"Error: Unable to create records in datahub. {response.content}")
+    # weather = weather.set_index(['latitude', 'longitude'])
+    # print(weather.head(10).to_string())
